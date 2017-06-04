cf8aa528c787c0813662a81d0287b50f1af61117
Daniel <Daniel@192.168.2.100>
2017-06-04 14:54:58 +0200

    Überflüssige Dateien gelöscht

--- ST2_Lager/src/main/java/de/lager/repositories/BestandsaenderungRepository.java
+++
@@ -1,9 +0,0 @@
-package de.lager.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import de.lager.entities.Bestandsaenderung;
-
-public interface BestandsaenderungRepository extends CrudRepository<Bestandsaenderung,Long> {
-
-}
